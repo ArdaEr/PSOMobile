49954ea8a9a24bf62f9672278f80ad8b88427c70
ArdaEr <cardaerdogan@hotmail.com>
2022-04-23 22:42:19 +0300

    Fixed Hit Bug

--- Assets/Scripts/BallForceScript.cs
+++ Assets/Scripts/BallForceScript.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallForceScript : MonoBehaviour
 {
     Rigidbody rigid = new Rigidbody();
+    private void Start() {
+        rigid = GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     private void OnMouseDown()
     {
-        rigid.AddForce(-transform.forward * 1000, ForceMode.Acceleration);
+        rigid.AddForce(transform.forward * 100, ForceMode.Acceleration);
         rigid.useGravity = true;
         Debug.Log("Vurdu");
+    }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            rigid.AddForce(transform.right * 1000, ForceMode.Acceleration);
+            rigid.useGravity = true;
+            Debug.Log("Vurdu");
+        }
     }
 }
