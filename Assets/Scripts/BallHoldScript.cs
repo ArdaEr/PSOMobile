a7aaf4d9bb28fa8a33fdc627c24b61264613d6ad
ÖMER ÖZTİN <omeroztin@gmail.com>
2022-04-25 17:45:11 +0300

    Shooting Mechanics Changed & Added Sliders

--- Assets/Scripts/BallHoldScript.cs
+++ Assets/Scripts/BallHoldScript.cs
@@ -1,47 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallHoldScript : MonoBehaviour
 {
     [SerializeField] Ball ball;
 
+    [SerializeField]private Slider shotbar;
+    [SerializeField]private Slider passbar;
 
-    private float holdDownStartTime;
+    public float timer = 0f;
+
+    private void Awake()
+    {
+        //slider = gameObject.GetComponent<Slider>();    
+    }
 
     // Update is called once per frame
     private void Update()
     {
+        
         if (Input.GetMouseButton(0))
         {
-            holdDownStartTime = Time.time;
-
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            //float holdDownTime = Time.time - holdDownStartTime;
-            //ball.Launch(CalculateHoldDownForce(holdDownTime));
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            //float holdDownTime = Time.time - holdDownStartTime;
-            ball.Launch(6f);
-
+            timer += Time.deltaTime * 1.8f;
+            shotbar.value = timer;
         }
         if (Input.GetMouseButton(1))
         {
-            //float holdDownTime = Time.time - holdDownStartTime;
-            ball.Launch(3f);
-
+            timer += Time.deltaTime * 1.2f;
+            passbar.value = timer;
         }
-    }
-
-    private float CalculateHoldDownForce(float holdtime)
-    {
-        float maxForceHoldDownTime = 2f;
-        float holdTimeNormalized = Mathf.Clamp01(holdtime / maxForceHoldDownTime);
-        float force = holdTimeNormalized * Ball.MAX_FORCE;
-        return force;
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (timer>=1.2f)
+            {
+                timer = 1.2f;
+            }
+            ball.Launch(timer * 7f);
+            Debug.Log(timer);
+            timer = 0f;
+            shotbar.value = 0f;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (timer >= 0.75f)
+            {
+                timer = 0.75f;
+            }
+            ball.Launch(timer*4.2f);
+            Debug.Log(timer);
+            passbar.value = 0f;
+            timer = 0f;
+        }
     }
 }
